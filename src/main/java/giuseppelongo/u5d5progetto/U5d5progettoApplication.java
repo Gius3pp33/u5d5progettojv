b62ceb4d9a94245520ef8f6c5974706e9a063651
Gius3pp33 <peppe-2@live.it>
2024-09-06 11:40:11 +0200

    Initial commit

---
+++ src/main/java/giuseppelongo/u5d5progetto/U5d5progettoApplication.java
@@ -0,0 +1,13 @@
+package giuseppelongo.u5d5progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d5progettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d5progettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuseppelongo/u5d5progetto/U5d5progettoApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppelongo.u5d5progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d5progettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
